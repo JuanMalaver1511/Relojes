efdd1a02734b063aac771590a847499c711bf9c6
JuanMalaver1511 <96750905+JuanMalaver1511@users.noreply.github.com>
2022-01-23 11:24:12 -0500

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,39 @@
+
+
+AOS.init();
+
+let menu = document.getElementById('menu');
+let menu_bar = document.getElementById('menu-bar');
+
+menu_bar.addEventListener('click', function(){
+    menu.classList.toggle('menu-toggle')
+})
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
